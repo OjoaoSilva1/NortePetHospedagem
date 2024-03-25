fbc84b30582e6f87469b901bd082386ac68ce7a0
Thiago-Muller150 <thiagomuller150@gmail.com>
2024-03-25 11:44:59 -0400

    a

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -13,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#D39C69',
     alignItems: 'center',
     justifyContent: 'center',
   },
